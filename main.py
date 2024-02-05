4c9d2d5793491fb1b168559afdb8c90425019d6b
suhaylp <71238928+suhaylp@users.noreply.github.com>
2024-02-04 17:17:00 -0800

    Need to comlete barkingburg

--- main.py
+++ main.py
@@ -72,13 +72,59 @@ def katies_q3(attempts):
         katies_q3(attempts - 1)
 
 
+
 def barkingburg():
-    print("Welcome to Barkingburg! This mission is under construction.")
-    # TODO: Implement Barkingburg mission
+    print("Welcome to barkingburg, isn't it just stunning. Sadly, we aren't here for a pleasant trip, we are here for a mission. I ryder have chosen you because of your impressive skill sets, and your smarts. Someone has stolen the princesses tiara! It is your duty to find it for the princess. SHe is depending on you. This may seem like a daunting quest, but don't worry, we know where the crown is. This is because it has a secret location tracker hidden in one of the diamonds. THe tracker shows that she is in the vault room, which is an enclosed room. ")
+    print ("The bad news is that the vault room is locked, and to open it you hae to answer three very difficult riddles. You only have 3 tries per question, use them wisely. Once you do that the room will open.")
+    barkingburg_q1(3)
+
+#TODO complete barkingburg, questions, and wirte an ending message, and finish some of the story line, for guidance look at the document you made. 
+
+def barkingburg_q1(attempts):
+    if attempts == 0:
+        print("You've run out of attempts! Let's try a different mission.")
+        place_chooser()
+        return
+    q1 = input("ENTER QUESTION HERE").lower()
+    if "" in q1 or "x" in q1 or "x" in q1 or "x" in q1:
+        print("Wow! You got this question right, here is the next question: ")
+        barkingburg_q2(attempts)
+    else:
+        print(f"Sorry, you got that wrong! You only have {attempts - 1} more attempt(s) on this question!")
+        barkingburg_q1(attempts - 1)
+
+
+def barkingburg_q2(attempts):
+    if attempts == 0:
+        print("You've run out of attempts! Let's try a different mission.")
+        place_chooser()
+        return
+    q2 = input("ENTER QUESTION HERE").lower()
+    if "x" in q2 or "x" in q2:
+        print ("Awesome, you got it right again!")
+        barkingburg_q3(attempts)
+    else:
+        print(f"Good try, you only have {attempts - 1} more attempt(s) on this question!")
+        barkingburg_q2(attempts - 1)
+
+
+def barkingburg_q3(attempts):
+    if attempts == 0:
+        print("You've run out of attempts! Let's try a different mission.")
+        place_chooser()
+        return
+    q3 = input("ENTER QUESTION HERE").lower()
+    if "x" in q3 or "x" in q3 or "x" in q3:
+        print(f"x") 
+    else:
+        print(f"Wrong! You only have {attempts - 1} more attempt(s) on this question!")
+        barkingburg_q3(attempts - 1)
+
 
 
 def foggybottom():
-    print("Welcome to Foggy Bottom. Mayor Humdinger wants to take over Adventure Bay, "
+    print(
+        "Welcome to Foggy Bottom. Mayor Humdinger wants to take over Adventure Bay, "
           "and what you need to do to stop him is to prevent his boat from picking him up so he can't cross into Adventure Bay."
           "\nYou see Wally in the sea, and since Wally's owner is Cap'n Turbot who is steering the ship, he can tell him to stop the boats. "
           "\n Wa;;y says he will only complete your request if you get im 3 squid peices. You run to the nearest fish market to get Wally his treats but you realize you don't have any money on you."
@@ -122,7 +168,7 @@ def foggy_q3(attempts):
     q3 = input("What has keys, but can't open any doors? ").lower()
     if "piano" in q3 or "keyboard" in q3 or "grand" in q3:
         print(f"That's impressive! I really didn't think you would get. Here is the octopus leg. Go give it Wally quickly!") 
-        print("You run ti give it wally, and he happily eats it, and swims to captain turbot.\n You wonder how you will know if wally kep this promise, and told captain turbot to turn the boat around, but then you see the boat turn around, and cruise of away from fogg bottom.")
+        print("You run tp give it wally, and he happily eats it, and swims to captain turbot.\n You wonder how you will know if wally tells captain turbot to turn the boat around, but then you see the boat turn around, and cruise of away from fogg bottom.")
         print("Ryder comes up to you and tells you, 'Good job, You have completed your mission we are all very proud of you. We know delcare you an honorary member of the paw patrol.' ")
     else:
         print(f"Wrong! You only have {attempts - 1} more attempt(s) on this question!")
